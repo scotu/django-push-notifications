bb1588605fc4e3b93e54f6f4924b964c376b857e
Daniel França <daniel.franca@gmail.com>
2013-04-26 16:15:56 -0300

    import models in __init__

--- push_notifications/__init__.py
+++ push_notifications/__init__.py
@@ -2,5 +2,7 @@ from django.conf import settings
 
 PUSH_NOTIFICATIONS_SETTINGS = getattr(settings, "PUSH_NOTIFICATIONS_SETTINGS", {})
 
+from models import *
+
 class NotificationError(Exception):
 	pass
